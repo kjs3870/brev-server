6ce50b73c0211759b0e91537e62971283c6e264e
jongseong <kjs3870@naver.com>
2020-11-24 10:09:31 +0900

    git repository 등록 라우터

--- server/routes/api.ts
+++ server/routes/api.ts
@@ -2,11 +2,13 @@ import express from "express";
 import authRouter from "./auth";
 import signUpRotuer from "./signup";
 import movieRouter from "./movie";
+import repoRouter from "./repo";
 
 const router: express.Router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/signup", signUpRotuer);
 router.use("/movie", movieRouter);
+router.use("/repo", repoRouter);
 
 export default router;
---
+++ server/routes/repo.ts
@@ -0,0 +1,20 @@
+import express, { Response } from "express";
+import isAuth from "../my_modules/middleware/isAuth";
+import userRequest from "../interface/request";
+import Repo from "../sequelize/models/repo.model";
+
+const router: express.Router = express.Router();
+
+router.post("/", isAuth, async (req: userRequest, res: Response) => {
+  const repo = req.body as Repo;
+  repo.userEmail = req.user.email;
+
+  try {
+    const r = await Repo.create(repo);
+    res.status(200).send(r);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+export default router;
